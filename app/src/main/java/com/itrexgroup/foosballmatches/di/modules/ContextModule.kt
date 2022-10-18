d8a46d600c24a76b60b7a90ac5bcd6bc742dc5b0
Korotkevich <alexey.korotkevich@itrexgroup.com>
2022-10-18 17:18:44 +0300

    Refactoring

--- app/src/main/java/com/itrexgroup/foosballmatches/di/modules/ContextModule.kt
+++ app/src/main/java/com/itrexgroup/foosballmatches/di/modules/ContextModule.kt
@@ -2,15 +2,14 @@ package com.itrexgroup.foosballmatches.di.modules
 
 import android.app.Application
 import android.content.Context
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-object ContextModule {
+abstract class ContextModule {
 
-    @Provides
+    @Binds
     @Singleton
-    @JvmStatic
-    fun provideAppContext(application: Application): Context = application.applicationContext
+    abstract fun bindAppContext(application: Application): Context
 }
